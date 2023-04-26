a1da4a3b8fb77867b89f1fb55f6216d4105dafb2
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2023-04-26 11:31:19 +0530

    0.2

--- App.js
+++ App.js
@@ -9,6 +9,12 @@ const Offers=require('./Controller/Offers')
 const Staffs=require('./Controller/Staffs')
 const bodyParser=require('body-parser')
 const mongoose=require('mongoose')
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://crystal-parts.web.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 require('./db')
 app.use(express.static('Public'))
 const cors=require('cors')
